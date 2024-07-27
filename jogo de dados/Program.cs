4e3026034c57e719c3d4d4aa4c19e1fc3e077cf3
FelipeMarinelli <98427995+FelipeMarinelli@users.noreply.github.com>
2024-07-27 20:15:10 -0300

    Add files via upload
    first commit

---
+++ jogo de dados/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogo_de_dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DicePlayer;
+            int AI;
+            int p1 = 0; 
+            int p2 = 0;
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Pressione uma tecla para girar os dados");
+
+                Console.ReadKey();
+
+                DicePlayer = random.Next(1, 7);
+                AI = random.Next(1, 7);
+
+                Console.WriteLine("Voce girou um " + DicePlayer);
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("A IA girou um " + AI);
+                if (AI > DicePlayer)
+                {
+                    p2++;
+                }
+                else
+                {
+                    p1++;
+                }
+                Console.WriteLine("pontuacao AI = " + p2 + " voce " + p1);
+
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
